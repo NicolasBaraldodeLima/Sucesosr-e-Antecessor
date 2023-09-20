7fe14cdf0e1bc8470ca4e5b2dea127662aab9581
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-09-20 14:50:43 -0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+
+
+
+$num = 15;
+
+
+echo "O antecessor de $num é " . ($num - 1) . " \n";
+echo "O sucessor de $num é " . ($num + 1). "\n";
